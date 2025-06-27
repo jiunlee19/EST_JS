23f26f9b266602b1e865e05ee1a3757adb1da151
jiunlee19 <tearofglass@gachon.ac.kr>
2025-06-27 16:05:40 +0900

    day3_quiz.js 수정

--- day3/과제/day3_quiz.js
+++ day3/과제/day3_quiz.js
@@ -1,9 +1,9 @@
 const heights = [150, 169, 185, 170];
 const ghost = 167;
 
-// ghost보다 키 큰 사람 출력
-const bggrGhost = heights.filter(height => height>=ghost);
-console.log(bggrGhost);
+// ghost보다 키 큰 사람 명수를 출력
+const bggrGhost = heights.filter(height => height >= ghost).length;
+console.log(bggrGhost+'명');
 
 const words = ['hello','ghost','apple','grape','Lemon'];
 // l자가 들어가있는 배열로 저장하시오. 대소문자 상관없이 출력.
